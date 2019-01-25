6d37e322ae125cfe1484e93a86ad3ffc1a75e997
Göran Olson <goran_6969@outlook.com>
2019-01-25 13:58:11 +0100

    Testvehicle created

--- MvcGarage2/Data/MvcGarage2Context.cs
+++ MvcGarage2/Data/MvcGarage2Context.cs
@@ -13,6 +13,25 @@ namespace MvcGarage2.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<ParkedVehicle>()
+               .HasData(
+                new { Id = 1, RegistrationNumber = "ABC123", Brand = "Volvo", VehicleModel = "V70", NumberOfWheels = 4, StartTime = DateTime.Parse("2019-01-10 12:00"), VehicleType = (VehicleType)0, Color = (ConsoleColor)0 },
+                new { Id = 2, RegistrationNumber = "BCD123", Brand = "Volvo", VehicleModel = "S80", NumberOfWheels = 4, StartTime = DateTime.Parse("2019-01-10 12:10"), VehicleType = (VehicleType)0, Color = (ConsoleColor)9 },
+                new { Id = 3, RegistrationNumber = "CDE123", Brand = "SAAB", VehicleModel = "900", NumberOfWheels = 4, StartTime = DateTime.Parse("2019-01-10 12:20"), VehicleType = (VehicleType)0, Color = (ConsoleColor)7 },
+
+                new { Id = 4, RegistrationNumber = "ABC001", Brand = "Yamaha", VehicleModel = "ZX750", NumberOfWheels = 2, StartTime = DateTime.Parse("2019-01-10 12:30"), VehicleType = (VehicleType)1, Color = (ConsoleColor)0 },
+                new { Id = 5, RegistrationNumber = "ABC002", Brand = "BMW", VehicleModel = "CC750", NumberOfWheels = 2, StartTime = DateTime.Parse("2019-01-10 12:40"), VehicleType = (VehicleType)1, Color = (ConsoleColor)9 },
+                new { Id = 6, RegistrationNumber = "ABC003", Brand = "BMW", VehicleModel = "CC900", NumberOfWheels = 2, StartTime = DateTime.Parse("2019-01-10 12:50"), VehicleType = (VehicleType)1, Color = (ConsoleColor)15 },
+
+                new { Id = 7, RegistrationNumber = "AOO111", Brand = "Scania", VehicleModel = "1200 KK", NumberOfWheels = 6, StartTime = DateTime.Parse("2019-01-10 13:00"), VehicleType = (VehicleType)2, Color = (ConsoleColor)15 },
+                new { Id = 8, RegistrationNumber = "AOO222", Brand = "Volvo", VehicleModel = "1200 KK", NumberOfWheels = 6, StartTime = DateTime.Parse("2019-01-10 13:10"), VehicleType = (VehicleType)2, Color = (ConsoleColor)12 },
+                new { Id = 9, RegistrationNumber = "AOO333", Brand = "Mercedes", VehicleModel = "1200 KK", NumberOfWheels = 4, StartTime = DateTime.Parse("2019-01-10 13:20"), VehicleType = (VehicleType)2, Color = (ConsoleColor)15 }
+                );
+       }
+
         public DbSet<MvcGarage2.Models.ParkedVehicle> ParkedVehicle { get; set; }
     }
 }
