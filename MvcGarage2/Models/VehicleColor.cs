e4ccfbbd78ed59e782772ecdf30782c816ecc1bb
Jon-blk <jon.svenonius@gmail.com>
2019-01-28 13:20:59 +0100

    Migrated colors to new enum + mapping that to views

---
+++ MvcGarage2/Models/VehicleColor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcGarage2.Models
+{
+    public enum VehicleColor
+    {
+        [Display(Name = "Svart")]
+        Black,
+        [Display(Name = "Mörkblå")]
+        DarkBlue,
+        [Display(Name = "Mörkgrön")]
+        DarkGreen,
+        [Display(Name = "Mörkröd")]
+        DarkRed,
+        [Display(Name = "Mörkgul")]
+        DarkYellow,
+        [Display(Name = "Grå")]
+        Grey,
+        [Display(Name = "Mörkgrå")]
+        DarkGrey,
+        [Display(Name = "Blå")]
+        Blue,
+        [Display(Name = "Grön")]
+        Green,
+        [Display(Name = "Röd")]
+        Red,
+        [Display(Name = "Gul")]
+        Yellow,
+        [Display(Name = "Vit")]
+        White,
+        [Display(Name = "Silver")]
+        Silver,
+        [Display(Name = "Guld")]
+        Gold,
+    }
+}
